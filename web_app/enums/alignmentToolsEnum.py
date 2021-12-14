ffd667d98bac91f7839e19d7274f07b7aa822b7b
luizmvb <luizmig52@gmail.com>
2021-12-14 08:25:15 -0300

    UPDATE alignmentToolsEnum and ADD requirements.txt

--- web_app/enums/alignmentToolsEnum.py
+++ web_app/enums/alignmentToolsEnum.py
@@ -3,4 +3,4 @@ from enum import Enum
 class AlignmentToolsEnum(Enum):
     NEEDLEMAN_WUNSCH = "Needleman-Wunsch"
     SMITH_WATERMAN = "Smith-Waterman"
-    STRIPED_SMITH_WATERMAN = "Striped Smith Waterman"
+    STRIPED_SMITH_WATERMAN = "Striped Smith-Waterman"
