5748bffcefa0f1e54afc7b8279bdf432cd07e1d7
luizmvb <luizmig52@gmail.com>
2022-03-15 08:40:25 -0300

    Changing initialization of application structure

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from web_app import app, db
-import resources
 from web_app.schema import Sequence, Taxonomy
 
 if __name__ == '__main__':
     db.create_all()
-    #resources.add_resources()
     app.run()
